1f41f755e61193893e1a59cce7a93c85fc4327c9
Yingqiao Xiong <yingqiaoxiong@d-172-25-248-175.dhcp.virginia.edu>
2019-12-05 23:18:11 -0500

    debbug and add more test

--- IntervalAnalysis/build.sh
+++ IntervalAnalysis/build.sh
@@ -1,3 +1,4 @@
-cd /Users/sparklingyo/compilers_proj/llvm-tutor/build
+export LLVM_DIR=/usr/local/opt/llvm/
+cd /Users/yingqiaoxiong/compiler/llvm-tutor/build/
 cmake -DLT_LLVM_INSTALL_DIR=$LLVM_DIR ../IntervalAnalysis/
 make
--- IntervalAnalysis/compile.sh
+++ IntervalAnalysis/compile.sh
@@ -1,4 +1,4 @@
-echo $LLVM_DIR
+export LLVM_DIR=/usr/local/opt/llvm
 
 $LLVM_DIR/bin/clang -S -emit-llvm ../inputs/loop_phi.cpp -o loop_phi.ll
 
